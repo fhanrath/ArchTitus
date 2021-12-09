4a9c56fa27d3fbff907f772ebc45a2073bd328fa
fenris <fabian.hanrath@posteo.de>
2021-12-09 20:14:29 +0100

    disable swap to save time and writecycles

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -178,21 +178,22 @@ fi
 echo -ne "
 -------------------------------------------------------------------------
                     Checking for low memory systems <24G
+                    DISABLED
 -------------------------------------------------------------------------
 "
-TOTALMEM=$(cat /proc/meminfo | grep -i 'memtotal' | grep -o '[[:digit:]]*')
-if [[  $TOTALMEM -lt 24000000 ]]; then
+#TOTALMEM=$(cat /proc/meminfo | grep -i 'memtotal' | grep -o '[[:digit:]]*')
+#if [[  $TOTALMEM -lt 24000000 ]]; then
     #Put swap into the actual system, not into RAM disk, otherwise there is no point in it, it'll cache RAM into RAM. So, /mnt/ everything.
-    mkdir /mnt/opt/swap #make a dir that we can apply NOCOW to to make it btrfs-friendly.
-    chattr +C /mnt/opt/swap #apply NOCOW, btrfs needs that.
-    dd if=/dev/zero of=/mnt/opt/swap/swapfile bs=1M count=16384 status=progress
-    chmod 600 /mnt/opt/swap/swapfile #set permissions.
-    chown root /mnt/opt/swap/swapfile
-    mkswap /mnt/opt/swap/swapfile
-    swapon /mnt/opt/swap/swapfile
-    #The line below is written to /mnt/ but doesn't contain /mnt/, since it's just / for the sysytem itself.
-    echo "/opt/swap/swapfile	none	swap	sw	0	0" >> /mnt/etc/fstab #Add swap to fstab, so it KEEPS working after installation.
-fi
+#    mkdir /mnt/opt/swap #make a dir that we can apply NOCOW to to make it btrfs-friendly.
+#    chattr +C /mnt/opt/swap #apply NOCOW, btrfs needs that.
+#    dd if=/dev/zero of=/mnt/opt/swap/swapfile bs=1M count=16384 status=progress
+#    chmod 600 /mnt/opt/swap/swapfile #set permissions.
+#    chown root /mnt/opt/swap/swapfile
+#    mkswap /mnt/opt/swap/swapfile
+#    swapon /mnt/opt/swap/swapfile
+#    #The line below is written to /mnt/ but doesn't contain /mnt/, since it's just / for the sysytem itself.
+#    echo "/opt/swap/swapfile	none	swap	sw	0	0" >> /mnt/etc/fstab #Add swap to fstab, so it KEEPS working after installation.
+#fi
 echo -ne "
 -------------------------------------------------------------------------
                     SYSTEM READY FOR 1-setup.sh
