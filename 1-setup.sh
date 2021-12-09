5e007bb8323686c33d95485398a95566bec8fdc9
fenris <fabian.hanrath@posteo.de>
2021-12-09 01:32:13 +0100

    amd drivers

--- 1-setup.sh
+++ 1-setup.sh
@@ -99,7 +99,7 @@ if lspci | grep -E "NVIDIA|GeForce"; then
     pacman -S nvidia --noconfirm --needed
 	nvidia-xconfig
 elif lspci | grep -E "Radeon"; then
-    pacman -S xf86-video-amdgpu --noconfirm --needed
+    pacman -S lib32-mesa xf86-video-amdgpu amdvlk lib32-amdvlk --noconfirm --needed
 elif lspci | grep -E "Integrated Graphics Controller"; then
     pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils --needed --noconfirm
 fi
