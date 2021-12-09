a5d4568f5104f87e92bfd03afa5bc9e9483e4443
fenris <fabian.hanrath@posteo.de>
2021-12-09 20:41:24 +0100

    install my layout

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -169,6 +169,12 @@ cp -R ${SCRIPT_DIR} /mnt/root/${SCRIPTHOME}
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 echo -ne "
 -------------------------------------------------------------------------
+                    Install custom layout
+-------------------------------------------------------------------------
+"
+cp ${SCRIPT_DIR}/keymap/us-intl.map.gz /mnt/usr/share/kbd/keymaps/i386/us-intl.map.gz
+echo -ne "
+-------------------------------------------------------------------------
                     GRUB BIOS Bootloader Install & Check
 -------------------------------------------------------------------------
 "
